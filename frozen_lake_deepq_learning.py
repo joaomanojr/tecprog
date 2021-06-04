1d5eba2954237102b583bcdd282ce3c0f860f27b
Joao Mano <joaomanojr@gmail.com>
2021-06-04 17:19:12 -0300

    Use homogeneous variable names
    Signed-off-by: Joao Mano <joaomanojr@gmail.com>

--- frozen_lake_deepq_learning.py
+++ frozen_lake_deepq_learning.py
@@ -5,32 +5,30 @@ from deepq_learning_agent import Agent
 
 if __name__ == '__main__':
 
-  env = gym.make('FrozenLake-v0', is_slippery = False)
-  #env = gym.make('FrozenLake-v0')
+  # TODO(joao): remove is_slippery = False when NN is improved
+  env = gym.make('FrozenLake-v0', is_slippery=False)
 
   n_games = 2000
   scores = []
   win_pct_list = []
   batch_size = 100
 
-  agent = Agent(lr = 0.0005, state_space = env.observation_space.n, action_space = env.action_space.n)
+  agent = Agent(lr=0.0005, state_space=env.observation_space.n, action_space=env.action_space.n)
 
   for i in range(n_games):
     score = 0
     done = False
-    # This looks like a context pointer to created environment, here we are
-    # just initializing it - it returns our current state also;
-    obs = env.reset()
+    # Initialize environment
+    state = env.reset()
 
     # Interact with environment until done (end or fall in a hole)
     while not done:
-      action = agent.choose_action(obs)
-      obs_, reward, done, info = env.step(action)
+      action = agent.choose_action(state)
+      state_, reward, done, info = env.step(action)
       #env.render()
       score += reward
-      #agent.learn(obs, action, reward, obs_, done)
-      agent.memory.append((obs, action, reward, obs_, done))
-      obs = obs_
+      agent.memory.append((state, action, reward, state_, done))
+      state = state_
     scores.append(score)
 
     if len(agent.memory) > batch_size:
