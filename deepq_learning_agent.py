6e0645346b92f7ff0973c59b6f1a310762d7c8f4
Joao Mano <joaomanojr@gmail.com>
2021-06-04 17:20:47 -0300

    Enforce pep8
    run autopep8 over design files:
    $ autopep8 <filenames> --max-line-length=100 --in-place
    
    Signed-off-by: Joao Mano <joaomanojr@gmail.com>

--- deepq_learning_agent.py
+++ deepq_learning_agent.py
@@ -8,149 +8,151 @@ import torch as T
 from collections import deque
 import random
 
+
 class LinearDeepQNetwork(nn.Module):
-  def __init__(self, lr, input, n_actions):
-    super(LinearDeepQNetwork, self).__init__()
+    def __init__(self, lr, input, n_actions):
+        super(LinearDeepQNetwork, self).__init__()
 
-    self.fc1 = nn.Linear(input, 128)
-    self.fc2 = nn.Linear(128, n_actions)
+        self.fc1 = nn.Linear(input, 128)
+        self.fc2 = nn.Linear(128, n_actions)
 
-    # Author: self.parameters() from inherited class Module
-    self.optimizer = optim.Adam(self.parameters(), lr=lr)
-    self.loss = nn.MSELoss()
-    self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
-    # Author: pytorch have different tensors for cuda/cpu devices
-    self.to(self.device)
+        # Author: self.parameters() from inherited class Module
+        self.optimizer = optim.Adam(self.parameters(), lr=lr)
+        self.loss = nn.MSELoss()
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
+        # Author: pytorch have different tensors for cuda/cpu devices
+        self.to(self.device)
 
-  def forward(self, state):
-    layer1 = F.relu(self.fc1(state))
-    # Author: MSELoss will take care of activation for us...
-    actions = self.fc2(layer1)
+    def forward(self, state):
+        layer1 = F.relu(self.fc1(state))
+        # Author: MSELoss will take care of activation for us...
+        actions = self.fc2(layer1)
 
-    return actions
+        return actions
 
 
 class Agent():
-  def __init__(self, lr, state_space, action_space, gamma=0.90,
-               epsilon=1.0, eps_dec=1e-5, eps_min=0.01):
-    """ Agent init takes:
-    --
-    lr - alpha learning rate factor
-    state_space - environment state space dimension
-    action_space - environment actions space dimension
-    gamma - discount factor on MDP rewards
-    epsilon - Epsilon Greedy initial value (exploration threshold)
-    eps_dec - Epsilon Greedy decrease factor
-    eps_min - Epsilon Greedy minimum, final value (must be > 0)
-      
-    """
-    self.lr = lr
-    self.input_dims = state_space
-    self.n_actions = action_space
-    self.gamma = gamma
-    self.epsilon = epsilon
-    self.eps_dec = eps_dec
-    self.eps_min = eps_min
-    self.action_space = [i for i in range(self.n_actions)]
-    self.np_arrays = []
-    for i in range(self.input_dims):
-      self.np_arrays.append(self.one_hot_state(i))
+    def __init__(self, lr, state_space, action_space, gamma=0.90,
+                 epsilon=1.0, eps_dec=1e-5, eps_min=0.01):
+        """ Agent init takes:
+        --
+        lr - alpha learning rate factor
+        state_space - environment state space dimension
+        action_space - environment actions space dimension
+        gamma - discount factor on MDP rewards
+        epsilon - Epsilon Greedy initial value (exploration threshold)
+        eps_dec - Epsilon Greedy decrease factor
+        eps_min - Epsilon Greedy minimum, final value (must be > 0)
 
-    self.memory = deque(maxlen=2000)
+        """
+        self.lr = lr
+        self.input_dims = state_space
+        self.n_actions = action_space
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.eps_dec = eps_dec
+        self.eps_min = eps_min
+        self.action_space = [i for i in range(self.n_actions)]
+        self.np_arrays = []
+        for i in range(self.input_dims):
+            self.np_arrays.append(self.one_hot_state(i))
 
-    self.Q = LinearDeepQNetwork(self.lr, self.input_dims, self.n_actions)
+        self.memory = deque(maxlen=2000)
 
-  def one_hot_state(self, state):
-    state_m = np.zeros((1, self.input_dims))
-    state_m[0][state] = 1
-    return state_m
+        self.Q = LinearDeepQNetwork(self.lr, self.input_dims, self.n_actions)
 
-  def choose_action(self, observation):
-    ''' Choose Epsilon Greedy action for a given state '''
-    rand_ = np.random.random()
+    def one_hot_state(self, state):
+        state_m = np.zeros((1, self.input_dims))
+        state_m[0][state] = 1
+        return state_m
 
-    if rand_ > self.epsilon:
-      state = T.tensor(self.np_arrays[observation], dtype=T.float).to(self.Q.device)
-      ## print(f'state is {state}')
-      ## print(f'state is {state.unsqueeze(dim=0)}')
-      # https://stackoverflow.com/questions/64192810/runtime-error-both-arguments-to-matmul-need-to-be-at-least-1d-but-they-are-0d
-      actions = self.Q.forward(state.unsqueeze(dim=0))
-      action = T.argmax(actions).item()
-    else:
-      action = np.random.choice(self.action_space)
-      ## print(f'random action {action}')
+    def choose_action(self, observation):
+        ''' Choose Epsilon Greedy action for a given state '''
+        rand_ = np.random.random()
 
-    return action
-    
-  def decrement_epsilon(self):
-    ''' Epsilon decrease function (linear) '''
-    # Look: my beloved C ternary in python terms!
-    self.epsilon = self.epsilon - self.eps_dec \
-                      if self.epsilon > self.eps_min else self.eps_min
+        if rand_ > self.epsilon:
+            state = T.tensor(self.np_arrays[observation], dtype=T.float).to(self.Q.device)
+            ## print(f'state is {state}')
+            ## print(f'state is {state.unsqueeze(dim=0)}')
+            # https://stackoverflow.com/questions/64192810/runtime-error-both-arguments-to-matmul-need-to-be-at-least-1d-but-they-are-0d
+            actions = self.Q.forward(state.unsqueeze(dim=0))
+            action = T.argmax(actions).item()
+        else:
+            action = np.random.choice(self.action_space)
+            ## print(f'random action {action}')
 
-  def learn(self, state, action, reward, state_, done):
-    """ Off Policy (always Greedy) Learn function 
-    --
-    Here defined as plain Bellman equation, state_ is state'
-    """
-    self.Q.optimizer.zero_grad()
-    state_T = T.tensor(self.np_arrays[state_], dtype=T.float).to(self.Q.device)
+        return action
 
-    #print(f'reward is {reward}')
-    if not done:
-      actions_T = self.Q.forward(state_T.unsqueeze(dim=0))
-      rewardT = reward + self.gamma * T.max(actions_T)
-    else:
-      rewardT = T.tensor(reward, dtype=T.float).to(self.Q.device)
+    def decrement_epsilon(self):
+        ''' Epsilon decrease function (linear) '''
+        # Look: my beloved C ternary in python terms!
+        self.epsilon = self.epsilon - self.eps_dec \
+            if self.epsilon > self.eps_min else self.eps_min
 
-    stateT = T.tensor(self.np_arrays[state], dtype=T.float).to(self.Q.device)
+    def learn(self, state, action, reward, state_, done):
+        """ Off Policy (always Greedy) Learn function 
+        --
+        Here defined as plain Bellman equation, state_ is state'
+        """
+        self.Q.optimizer.zero_grad()
+        state_T = T.tensor(self.np_arrays[state_], dtype=T.float).to(self.Q.device)
 
-    q_pred = self.Q.forward(stateT.unsqueeze(dim=0))
-    q_target = self.Q.forward(stateT.unsqueeze(dim=0))
-    q_target[0][0][action] = rewardT
-    loss = self.Q.loss(q_pred, q_target).to(self.Q.device)
+        #print(f'reward is {reward}')
+        if not done:
+            actions_T = self.Q.forward(state_T.unsqueeze(dim=0))
+            rewardT = reward + self.gamma * T.max(actions_T)
+        else:
+            rewardT = T.tensor(reward, dtype=T.float).to(self.Q.device)
 
-    # Author: backpropagate cost and add a step on our optimizer.
-    # These two calls are critical for learn loop.
-    loss.backward()
-    self.Q.optimizer.step()
+        stateT = T.tensor(self.np_arrays[state], dtype=T.float).to(self.Q.device)
 
-    self.decrement_epsilon()
+        q_pred = self.Q.forward(stateT.unsqueeze(dim=0))
+        q_target = self.Q.forward(stateT.unsqueeze(dim=0))
+        q_target[0][0][action] = rewardT
+        loss = self.Q.loss(q_pred, q_target).to(self.Q.device)
 
-  def batch_learn(self, batch_size):
-      minibatch = random.sample(self.memory, batch_size)
-      for state, action, reward, next_state, done in minibatch:
-        self.learn(state, action, reward, next_state, done)
+        # Author: backpropagate cost and add a step on our optimizer.
+        # These two calls are critical for learn loop.
+        loss.backward()
+        self.Q.optimizer.step()
 
-  def print_learn_snapshot(self):
-    """
-    Print a snapshot of learning situation
+        self.decrement_epsilon()
 
-    |S(.)0.589||F(>)0.653||F(.)0.727||F(<)0.668|
-    |F(.)0.655||H(.)0.677||F(.)0.809||H(.)0.637|
-    |F(>)0.728||F(>)0.810||F(.)0.900||H(>)0.808|
-    |H(.)0.684||F(>)0.898||F(>)0.997||G(.)0.657|
+    def batch_learn(self, batch_size):
+        minibatch = random.sample(self.memory, batch_size)
+        for state, action, reward, next_state, done in minibatch:
+            self.learn(state, action, reward, next_state, done)
 
-    Cell format: <status>(<best_action>)<best_action_value>
-      status      - 'S'=start, 'G'=goal, 'F'=frozen, 'H'=hole
-      best_action - '<'=start, '.'=down, '>'=right, '^'=up
-      best_value  - Extracted value for best_action from NN tensor
-    """
+    def print_learn_snapshot(self):
+        """
+        Print a snapshot of learning situation
 
-    print('\nLearn snapshot: ')
+        |S(.)0.589||F(>)0.653||F(.)0.727||F(<)0.668|
+        |F(.)0.655||H(.)0.677||F(.)0.809||H(.)0.637|
+        |F(>)0.728||F(>)0.810||F(.)0.900||H(>)0.808|
+        |H(.)0.684||F(>)0.898||F(>)0.997||G(.)0.657|
 
-    # actions:: LEFT = 0 DOWN = 1 RIGHT = 2 UP = 3
-    action_str = ['<', '.', '>', '^']
+        Cell format: <status>(<best_action>)<best_action_value>
+          status      - 'S'=start, 'G'=goal, 'F'=frozen, 'H'=hole
+          best_action - '<'=start, '.'=down, '>'=right, '^'=up
+          best_value  - Extracted value for best_action from NN tensor
+        """
 
-    map_str = []
-    map_str.append(['S', 'F', 'F', 'F'])
-    map_str.append(['F', 'H', 'F', 'H'])
-    map_str.append(['F', 'F', 'F', 'H'])
-    map_str.append(['H', 'F', 'F', 'G'])
+        print('\nLearn snapshot: ')
 
-    for line in range(4):
-      for col in range(4):
-        stateT = T.tensor(self.np_arrays[line * 4 + col], dtype=T.float).to(self.Q.device)
-        actionsT = self.Q.forward(stateT.unsqueeze(dim=0))
-        print(f'|{map_str[line][col]}({action_str[T.argmax(actionsT).item()]}){T.max(actionsT).item():4.3f}|', end='')
-      print('')
+        # actions:: LEFT = 0 DOWN = 1 RIGHT = 2 UP = 3
+        action_str = ['<', '.', '>', '^']
+
+        map_str = []
+        map_str.append(['S', 'F', 'F', 'F'])
+        map_str.append(['F', 'H', 'F', 'H'])
+        map_str.append(['F', 'F', 'F', 'H'])
+        map_str.append(['H', 'F', 'F', 'G'])
+
+        for line in range(4):
+            for col in range(4):
+                stateT = T.tensor(self.np_arrays[line * 4 + col], dtype=T.float).to(self.Q.device)
+                actionsT = self.Q.forward(stateT.unsqueeze(dim=0))
+                print(
+                    f'|{map_str[line][col]}({action_str[T.argmax(actionsT).item()]}){T.max(actionsT).item():4.3f}|', end='')
+            print('')
--- frozen_lake_deepq_learning.py
+++ frozen_lake_deepq_learning.py
@@ -5,40 +5,40 @@ from deepq_learning_agent import Agent
 
 if __name__ == '__main__':
 
-  # TODO(joao): remove is_slippery = False when NN is improved
-  env = gym.make('FrozenLake-v0', is_slippery=False)
+    # TODO(joao): remove is_slippery = False when NN is improved
+    env = gym.make('FrozenLake-v0', is_slippery=False)
 
-  n_games = 2000
-  scores = []
-  win_pct_list = []
-  batch_size = 100
+    n_games = 2000
+    scores = []
+    win_pct_list = []
+    batch_size = 100
 
-  agent = Agent(lr=0.0005, state_space=env.observation_space.n, action_space=env.action_space.n)
+    agent = Agent(lr=0.0005, state_space=env.observation_space.n, action_space=env.action_space.n)
 
-  for i in range(n_games):
-    score = 0
-    done = False
-    # Initialize environment
-    state = env.reset()
+    for i in range(n_games):
+        score = 0
+        done = False
+        # Initialize environment
+        state = env.reset()
 
-    # Interact with environment until done (end or fall in a hole)
-    while not done:
-      action = agent.choose_action(state)
-      state_, reward, done, info = env.step(action)
-      #env.render()
-      score += reward
-      agent.memory.append((state, action, reward, state_, done))
-      state = state_
-    scores.append(score)
+        # Interact with environment until done (end or fall in a hole)
+        while not done:
+            action = agent.choose_action(state)
+            state_, reward, done, info = env.step(action)
+            # env.render()
+            score += reward
+            agent.memory.append((state, action, reward, state_, done))
+            state = state_
+        scores.append(score)
 
-    if len(agent.memory) > batch_size:
-      agent.batch_learn(batch_size)
+        if len(agent.memory) > batch_size:
+            agent.batch_learn(batch_size)
 
-    if i % 100 == 0:
-      win_pct = np.mean(scores[-100:])
-      win_pct_list.append(win_pct)
-      print(f'episode {i} win pct {win_pct:.2f} epsilon {agent.epsilon:.2f}')
-      agent.print_learn_snapshot()
+        if i % 100 == 0:
+            win_pct = np.mean(scores[-100:])
+            win_pct_list.append(win_pct)
+            print(f'episode {i} win pct {win_pct:.2f} epsilon {agent.epsilon:.2f}')
+            agent.print_learn_snapshot()
 
-  plt.plot(win_pct_list)
-  plt.show()
+    plt.plot(win_pct_list)
+    plt.show()
